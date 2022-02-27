a439f3bf670571579db630dd4d9fdf3f8f7e2e48
Kirill Oleynik <kirill.olejnik@gmail.com>
2022-02-27 19:47:10 +0200

    Add Admin model spec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:admin) }
+
+  it { is_expected.to validate_presence_of(:email) }
 end
