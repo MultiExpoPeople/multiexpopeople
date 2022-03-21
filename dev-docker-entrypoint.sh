a93888c8165703c38af2e0fedae6ef9ca7e10eaa
Kirill Oleynik <kirill.olejnik@gmail.com>
2022-03-21 14:59:25 +0200

    Refacor docker files to work properly

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-
-#make sure that this scrpit exis if here is an error
+#make sure that scrpit exis if here is an error
 set -e
 
-echo "Environment: $RAILS_ENV"
-
-# install missing gems if needed
 bundle check || bundle install --jobs 20 --retry 5
-
-# Remove pre-existing puma/passenger server.pid
+bin/rails db:create db:migrate
 rm -f $APP_PATH/tmp/pids/server.pid
 
 # run passed commands prepanding 'bundle exec'
